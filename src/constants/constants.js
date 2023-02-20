4fc7318128d851de1a689df4b2d4415f7c09002c
André Dias <andredds12@gmail.com>
2023-02-20 22:50:55 +0100

    update prompt

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,8 +2,7 @@ export const PROMPTS = {
   INIT_PROMPT:
     "Act like a food reviewer and recommend to me 3 restaurants following the rules:",
   JSON_EXPLANATION:
-    "Just answer the compact JSON (without new lines and blank spaces) with a list of restaurants. The object restaurant contain the attributes are: name of the restaurant(restaurant_name), trip advisor rate (rate), location (location), meters from the actual location (far_from) and a text resuming all information as text including a description  of the menu/foods, if is possible recommend the best plate (maximum 50 words) (text).",
-};
+    "Just return the compact JSON (removing all new lines(\\n) and unnecessary blank spaces) with a list of restaurants. The object restaurant contain the attributes are: name of the restaurant(name), location (location), the distance from the actual location (far_from) and a text resuming all information as text including a description of the menu/foods, if is possible recommend the best plate (maximum 50 words) (text)"};
 
 export const WELCOME_QUESTIONS = [
   "Do you want restaurant recommendations?",
