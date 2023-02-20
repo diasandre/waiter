e4c762f6a91ec8529bece205e1da7c838a1b3b3a
André Dias <andredds12@gmail.com>
2023-02-20 22:57:59 +0100

    improve prompt

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,17 +3,17 @@ import { Answer } from "../styles";
 
 const Message = ({ message, index }) => {
   const time = `${index}.0`;
-  //   const farFrom =
-  //     Number(message.far_from) < 1
-  //       ? `${message.far_from}km`
-  //       : `${message.far_from} meters`;
+  const farFrom =
+    Number(message.far_from) < 5
+      ? `${message.far_from}km`
+      : `${message.far_from} meters`;
   return (
-    <div key={message.restaurant_name} className="answer-container">
+    <div key={message.name} className="answer-container">
       <Answer time={time}>
         {message.text}
         <div className="information-message">
-          <span className="restaurant-name">{message.restaurant_name}</span>
-          {` - ${message.far_from} from you - ${message.location}`}
+          <span className="restaurant-name">{message.name}</span>
+          {` - ${farFrom} from you - ${message.location}`}
         </div>
       </Answer>
     </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,8 +2,8 @@ export const PROMPTS = {
   INIT_PROMPT:
     "Act like a food reviewer and recommend to me 3 restaurants following the rules:",
   JSON_EXPLANATION:
-    "Just return the compact JSON (removing all new lines(\\n) and unnecessary blank spaces) with a list of restaurants. The object restaurant contain the attributes are: name of the restaurant(name), location (location), the distance from the actual location (far_from) and a text resuming all information as text including a description of the menu/foods, if is possible recommend the best plate (maximum 50 words) (text)"};
-
+    "Just return the compact JSON (removing all new lines(\\n) and unnecessary blank spaces) with a list of restaurants. The object restaurant contain the attributes are: name of the restaurant(name),  street, number and city (location), the distance from the actual location(in meters) (far_from) and a text resuming all information as text including a description of the menu/foods, if is possible recommend the best plate (maximum 50 words) (text).",
+};
 export const WELCOME_QUESTIONS = [
   "Do you want restaurant recommendations?",
   "Would you like some restaurant suggestions?",
